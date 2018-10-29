1a804f0d05eb3db57a17c416419e4e78f02f4903
Jon Ceanfaglione <jon.ceanfaglione@gmail.com>
2018-10-29 13:37:52 -0400

    added empty test

---
+++ src/components/TodoList/Main.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { shallow } from '../../enzyme';
+
+import TodosList from './Main';
+
+describe('Todos List Tests', () => {
+
+  it('renders empty-message', () => {
+
+    
+    
+  })
+
+});
